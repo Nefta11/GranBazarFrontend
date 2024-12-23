96cc160ad363da2fb4bbb5dc662b3698a3b5a2e7
Nefta11 <neftaliarturohernandez@gmail.com>
2024-12-23 15:22:26 -0600

    Agregar soporte para modo oscuro: definir variables CSS y ajustar estilos en footer, main-view y menú

--- src/components/MenuDesktop.jsx
+++ src/components/MenuDesktop.jsx
@@ -1,10 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../assets/styles/menu.css';
-import { FaHome, FaFemale, FaChild, FaMale, FaBars, FaUser, FaMoon, FaSun } from 'react-icons/fa';
+import { FaHome, FaFemale, FaChild, FaMale, FaBars, FaUser, FaSun, FaMoon } from 'react-icons/fa';
 
 const MenuDesktop = ({ logo }) => {
     const [menuActive, setMenuActive] = useState(false);
     const [darkMode, setDarkMode] = useState(false);
+
+    useEffect(() => {
+        const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+        setDarkMode(userPrefersDark);
+        document.body.classList.toggle('dark-mode', userPrefersDark);
+    }, []);
 
     const toggleMenu = () => {
         setMenuActive(!menuActive);
@@ -17,13 +23,13 @@ const MenuDesktop = ({ logo }) => {
 
     return (
         <div className="menu-desktop">
-            <button className="btn-dark-mode" onClick={toggleDarkMode}>
-                {darkMode ? <FaSun /> : <FaMoon />}
-            </button>
             <div className="container-logo">
                 <a href="/">
                     <img src={logo} alt="icono gran bazar" />
                 </a>
+                <button className="btn-dark-mode" onClick={toggleDarkMode}>
+                    {darkMode ? <FaSun /> : <FaMoon />}
+                </button>
             </div>
             <div className="container-buttons">
                 <h1 className="title-app">Gran Bazar</h1>
@@ -32,7 +38,7 @@ const MenuDesktop = ({ logo }) => {
                     <li><a href="/"><FaFemale /> Mujeres</a></li>
                     <li><a href="/"><FaMale /> Hombres</a></li>
                     <li><a href="/"><FaChild /> Niñ@s</a></li>
-                    <li><a href="/"><FaUser /> Mi Perfil</a></li>
+                    <li><a href="/"><FaUser/> Mi Perfil</a></li>
                 </ul>
                 <button className="btn-mobile-menu" onClick={toggleMenu}><FaBars /></button>
             </div>
