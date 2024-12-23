f4fa9189bfb3ae7fdc6efaa1a368f7eb98b91f31
Nefta11 <neftaliarturohernandez@gmail.com>
2024-12-23 11:59:41 -0600

    Eliminar estilos del footer en main-view.css y crear nuevos archivos para el componente Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import '../assets/styles/footer.css'; // Importa el archivo CSS específico para el footer
+
+const Footer = () => {
+    return (
+        <footer>
+            <section className="footer-desktop">
+                <div className="footer-item__desktop">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d4011.1913055687737!2d-97.9584259705053!3d20.238582122701505!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2smx!4v1691865537809!5m2!1sen!2smx"
+                        width="600"
+                        height="450"
+                        style={{ border: 0 }}
+                        allowFullScreen=""
+                        loading="lazy"
+                        referrerPolicy="no-referrer-when-downgrade"
+                        className="location"
+                        title="Ubicación Gran Bazar Desktop"
+                    ></iframe>
+                </div>
+                <div className="footer-item__desktop two">
+                    <p>Encuentranos en nuestras redes sociales</p>
+                    <div className="icons-footer-content">
+                        <i className='bx bxl-facebook-square'></i> <br />
+                        <i className='bx bxl-instagram-alt'></i> <br />
+                        <i className='bx bxl-tiktok'></i>
+                    </div>
+                </div>
+            </section>
+            <section className="footer-mobile">
+                <p className="texto-footer-mobile">Visitanos</p>
+                <div className="footer-item__desktop">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d4011.1913055687737!2d-97.9584259705053!3d20.238582122701505!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2smx!4v1691865537809!5m2!1sen!2smx"
+                        width="600"
+                        height="450"
+                        style={{ border: 0 }}
+                        allowFullScreen=""
+                        loading="lazy"
+                        referrerPolicy="no-referrer-when-downgrade"
+                        className="location"
+                        title="Ubicación Gran Bazar Mobile"
+                    ></iframe>
+                </div>
+            </section>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import logoGranBazar from '../assets/images/logo_gran_bazar.jpeg'; // Importa la
 import MenuDesktop from '../components/MenuDesktop';
 import MenuMobile from '../components/MenuMobile';
 import NavBar from '../components/NavBar';
+import Footer from '../components/Footer'; // Importa el nuevo componente Footer
 
 const Home = () => {
     return (
@@ -34,47 +35,7 @@ const Home = () => {
                     {/* Aquí puedes agregar los productos */}
                 </section>
             </main>
-            <footer>
-                <section className="footer-desktop">
-                    <div className="footer-item__desktop">
-                        <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d4011.1913055687737!2d-97.9584259705053!3d20.238582122701505!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2smx!4v1691865537809!5m2!1sen!2smx"
-                            width="600"
-                            height="450"
-                            style={{ border: 0 }}
-                            allowFullScreen=""
-                            loading="lazy"
-                            referrerPolicy="no-referrer-when-downgrade"
-                            className="location"
-                            title="Ubicación Gran Bazar Desktop"
-                        ></iframe>
-                    </div>
-                    <div className="footer-item__desktop two">
-                        <p>Encuentranos en nuestras redes sociales</p>
-                        <div className="icons-footer-content">
-                            <i className='bx bxl-facebook-square'></i> <br />
-                            <i className='bx bxl-instagram-alt'></i> <br />
-                            <i className='bx bxl-tiktok'></i>
-                        </div>
-                    </div>
-                </section>
-                <section className="footer-mobile">
-                    <p className="texto-footer-mobile">Visitanos</p>
-                    <div className="footer-item__desktop">
-                        <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d4011.1913055687737!2d-97.9584259705053!3d20.238582122701505!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2smx!4v1691865537809!5m2!1sen!2smx"
-                            width="600"
-                            height="450"
-                            style={{ border: 0 }}
-                            allowFullScreen=""
-                            loading="lazy"
-                            referrerPolicy="no-referrer-when-downgrade"
-                            className="location"
-                            title="Ubicación Gran Bazar Mobile"
-                        ></iframe>
-                    </div>
-                </section>
-            </footer>
+            <Footer /> {/* Utiliza el nuevo componente Footer */}
         </div>
     );
 };
