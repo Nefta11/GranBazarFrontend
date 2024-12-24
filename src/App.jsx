f994583ba186cb9f53408d8e5bab1af3abe74e27
Nefta11 <neftaliarturohernandez@gmail.com>
2024-12-23 18:29:58 -0600

    Eliminar el proveedor de Redux del componente App: simplificar la estructura del componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import store from './store/store';
 import Home from './pages/Home';
 import Login from './pages/Auth/Login';
 
 function App() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path='/Login' element={<Login />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/Login' element={<Login />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
